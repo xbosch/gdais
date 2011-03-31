ca6434629c225f9397232f632ed4456618342c88
pauharo@gmail.com <pauharo@gmail.com@4bc5d4ef-97bb-c318-395f-cf2583fdf7ac>
2011-03-31 12:19:34 +0000

    Reorganized and added some comments to read_data() function.

--- trunk/connection.py
+++ trunk/connection.py
@@ -45,6 +45,7 @@ class Connection(QThread):
             # suppose all packets of the same length as there's no end mark
             packet = instrument.rx_packets.values()[0]
             self.buffer_size = packet.struct.size # struct added by parser
+            
             if PacketFormat.FormatField.packet_num in format.rx_format:
                 self.buffer_size += 1
         
@@ -55,41 +56,73 @@ class Connection(QThread):
         self.log.debug("Ending connection thread")
     
     def send_data(self, data):
-        # implemented if connection can send data
-        raise NotImplementedError
+        format = self.instrument.packet_format
+        
+        if PacketFormat.FormatField.start_bytes in format.tx_format:
+            self.io_conn.write(bytearray(format.start_bytes))
+        
+        self.io_conn.write(str(data))
+        
+        if PacketFormat.Formatield.end_bytes in format.tx_format:
+            self.io_conn.write(bytearray(format.end_bytes))
     
     def read_data(self, fd):
         format = self.instrument.packet_format
         while True:
             data = self.io_conn.read(self.buffer_size)
             if not data:
+                # continue reading until some data is read
                 break
             
             if not self.packet:
+                # starting a new packet, using the remaining data from previous packet
                 if self.old_data:
                     data = self.old_data + data
                 if PacketFormat.FormatField.start_bytes in format.rx_format:
-                    for i in xrange(len(data) - len(format.start_bytes) + 1):
-                        if data[i:i + len(format.start_bytes)] == bytearray(format.start_bytes):
-                            self.packet += data[len(format.start_bytes) + i:]
+                    # search for packet start bytes in input data
+                    start_bytes = bytearray(format.start_bytes)
+                    for i in range(len(data) - len(format.start_bytes) + 1):
+                        if data[i:i + len(format.start_bytes)] == start_bytes:
+                            # packet start bytes found, append new data without start bytes
+                            self.packet += data[i + len(format.start_bytes):]
                             break
                 else:
+                    # no start bytes mark, just use this data as the packet start
                     self.packet += data
             else:
+                # already started a packet, append new data
                 self.packet += data
             
             if PacketFormat.FormatField.end_bytes in format.rx_format:
-                for i in xrange(len(data) - len(format.end_bytes) + 1):
-                    if data[-i - len(format.end_bytes):-i] == bytearray(format.end_bytes):
-                        self.new_data_received.emit(self.packet[:-i - len(format.end_bytes)])
-                        self.old_data = self.packet[-i:]
-                        self.packet = bytearray('')
+                # search for packet end bytes in input data
+                end_bytes = bytearray(format.end_bytes)
+                for i in range(len(data) - len(format.end_bytes) + 1):
+                    if data[-i - len(format.end_bytes):-i] == end_bytes:
+                        # Packet end bytes found, emit the new packet without end bytes,
+                        # store exceeding data and prepare for next packet
+                        self._new_packet_found(
+                            packet_data=self.packet[:-i - len(format.end_bytes)],
+                            excess_data=self.packet[-i:]
+                        )      
                         break
             else:
+                # Without end bytes marker just try to get a fixed length data bytes,
+                # store not used data and prepare for next packet
                 if len(self.packet) >= self.buffer_size:
-                    self.new_data_received.emit(self.packet[:self.buffer_size])
-                    self.old_data = self.packet[self.buffer_size:]
-                    self.packet = bytearray('')
+                    self._new_packet_found(
+                       packet_data=self.packet[:self.buffer_size],
+                       excess_data=self.packet[self.buffer_size:]
+                    )
+    
+    # TODO: try to get the end of the packet by searching a new packet start
+    
+    def _new_packet_found(self, packet_data, excess_data):
+        # emit the new packet data
+        self.new_data_received.emit(packet_data)
+        
+        # store not used data and prepare for next packet
+        self.old_data = excess_data
+        self.packet = bytearray('')
 
 
 class SerialConnection(Connection):
@@ -144,9 +177,6 @@ class SerialConnection(Connection):
         notifier.activated.connect(self.read_data)
         
         Connection.run(self)
-    
-    def send_data(self, data):
-        self.io_conn.write(str(data))
 
 
 class FileConnection(Connection):
@@ -170,3 +200,7 @@ class FileConnection(Connection):
         self.read_data(self.io_conn.fileno())
         
         Connection.run(self)
+    
+    def send_data(self, data):
+        # file connection can't send data
+        raise NotImplementedError
