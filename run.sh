297c5614d2d6ee0ded0815ad82abed4a055a1bed
pauharo@gmail.com <pauharo@gmail.com@4bc5d4ef-97bb-c318-395f-cf2583fdf7ac>
2011-05-26 11:46:38 +0000

    Improved TCP server, now it can receive control commands.

--- run.sh
+++ run.sh
@@ -13,5 +13,12 @@ if [ $# -eq 1 ]; then
   equipment="$1"
 fi
 
+# check no other GDAIS process is running
+ps -eo cmd | grep main.py | grep python > /dev/null
+if [ $? -eq 0 ]; then
+  echo "GDAIS already running, only one instance can be started."
+  exit 2
+fi
+
 # run GDAIS
 python main.py $equipment 2>&1 | tee debug.log | cut -b -100
