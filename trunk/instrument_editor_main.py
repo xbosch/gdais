692530103daeca4ea5ee70fe567487be751a317b
pauharo@gmail.com <pauharo@gmail.com@4bc5d4ef-97bb-c318-395f-cf2583fdf7ac>
2011-06-14 09:30:19 +0000

    Moved all GDAIS-core code into its subfolder

--- trunk/instrument_editor_main.py
+++
@@ -1,9 +0,0 @@
-from PyQt4 import QtCore, QtGui
-from ui.instrumenteditor_mainwindow import InstrumentEditorMainWindow
-
-if __name__ == "__main__":
-    import sys
-    app = QtGui.QApplication(sys.argv)
-    ui = InstrumentEditorMainWindow()
-    ui.show()
-    sys.exit(app.exec_())
