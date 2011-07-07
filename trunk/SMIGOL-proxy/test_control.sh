eafae93d70eb36af410e937d8f27e16bf6e4a5b5
pauharo@gmail.com <pauharo@gmail.com@4bc5d4ef-97bb-c318-395f-cf2583fdf7ac>
2011-07-07 13:30:03 +0000

    Changes to SMIGOL-proxy after first experiments.

--- trunk/SMIGOL-proxy/test_control.sh
+++ trunk/SMIGOL-proxy/test_control.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
-if [ ! "$1" ]; then
-  echo "usage: $(basename $0) [port]"
-  exit 1
-fi
+TCP_PORT=20000
 
-port="$1"
+# start proxy process
+echo "Starting proxy process..."
+python smigol.py &
+
+# wait for it to start
+sleep 1
 
 # try to connect
-if ! exec 3<> /dev/tcp/localhost/$port; then
-  echo "$(basename $0): unable to connect to localhost:$port"
+if ! exec 3<> /dev/tcp/localhost/$TCP_PORT; then
+  echo "$(basename $0): unable to connect to localhost:$TCP_PORT"
   exit 1
 fi
 
-echo "Press enter to send start command"
+# wait for control connection established
+sleep 1
+echo "Ready!"
+echo
+echo "Now start GDAIS and then press enter to send start command"
 read
 echo "Sending 'start'..."
 echo -n "s" 1>&3
-
+echo
 echo "Press enter to send quit command"
 read
 echo "Sending 'quit'..."
