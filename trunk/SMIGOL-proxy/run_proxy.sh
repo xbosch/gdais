665ed67c58d46eece9275fb4b0e440a31d28b660
pauharo@gmail.com <pauharo@gmail.com@4bc5d4ef-97bb-c318-395f-cf2583fdf7ac>
2011-07-11 20:51:36 +0000

    Modifications to enable the whole proxy and GDAIS system to be run just executing run_proxy.sh, which controls everything during the acquisition.

--- trunk/SMIGOL-proxy/run_proxy.sh
+++ trunk/SMIGOL-proxy/run_proxy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-TCP_PORT=20000
+PROXY_PORT=20000
+
+GDAIS_PATH="/home/pau/feina/UPC/projecte/code/GDAIS/GDAIS-core"
+GDAIS_PORT=12345
 
 # start proxy process
 echo "Starting proxy process..."
@@ -9,27 +12,55 @@ python smigol.py &
 # wait for it to start
 sleep 1
 
-# try to connect
-if ! exec 3<> /dev/tcp/localhost/$TCP_PORT; then
-  echo "$(basename $0): unable to connect to localhost:$TCP_PORT"
+# try to connect to the proxy
+if ! exec 3<> /dev/tcp/localhost/$PROXY_PORT; then
+  echo "$(basename $0): unable to connect to proxy localhost:$PROXY_PORT"
   exit 1
 fi
 
 # wait for control connection established
 sleep 1
-echo "Ready!"
+echo "proxy started!"
 echo
-echo "Now start GDAIS and then press enter to send start command"
-read
-echo "Sending 'start'..."
+
+echo "Now starting GDAIS..."
+$GDAIS_PATH/run.sh -d $GDAIS_PATH/conf/equips/smigol.json
+
+# wait for it to start
+sleep 2
+
+# try to connect to GDAIS
+if ! exec 4<> /dev/tcp/localhost/$GDAIS_PORT; then
+  echo "$(basename $0): unable to connect to GDAIS localhost:$GDAIS_PORT"
+  # stop proxy and clear connection
+  echo -n "s" 1>&3
+  echo -n "q" 1>&3
+  exec 3<&-
+  exit 2
+fi
+
+echo "Set GDAIS log level to WARNING"
+echo -n "set_log_level 30" 1>&4
+
+echo "GDAIS started!"
+echo 
+
+# wait more
+sleep 1
+
+echo "Sending 'start' to proxy..."
 echo -n "s" 1>&3
 echo
-echo "Press enter to send quit command"
-read
-echo "Sending 'quit'..."
-echo -n "q" 1>&3
 
-exec 3<&-
+echo "Waiting for proxy to finish..."
+wait
+echo "Proxy process exited"
+echo
 
+echo "Sending 'quit' to GDAIS..."
+echo -n "quit" 1>&4
+exec 4<&-
+
+echo "Finished!"
 exit 0
 
